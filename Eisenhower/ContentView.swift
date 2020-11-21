a3663072158380a30f42e8a2290fff04c25c834d
Louis Tsai <louis993546@gmail.com>
2020-11-21 01:02:19 +0100

    Initial Commit

---
+++ Eisenhower/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Eisenhower
+//
+//  Created by Louis Tsai on 11/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Eisenhower/EisenhowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EisenhowerApp.swift
+//  Eisenhower
+//
+//  Created by Louis Tsai on 11/21/20.
+//
+
+import SwiftUI
+
+@main
+struct EisenhowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
